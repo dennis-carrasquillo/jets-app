e7355005492ab156dbff7474a5f9c0ccf0c403b0
Dennis Carrasquillo <spycydennis@gmail.com>
2021-10-08 19:21:37 -0400

    Created the Spring backend

---
+++ backend/jets/src/main/java/com/dennis/carrasquillo/jets/JetsApplication.java
@@ -0,0 +1,13 @@
+package com.dennis.carrasquillo.jets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetsApplication.class, args);
+	}
+
+}
---
+++ backend/jets/src/test/java/com/dennis/carrasquillo/jets/JetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennis.carrasquillo.jets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
