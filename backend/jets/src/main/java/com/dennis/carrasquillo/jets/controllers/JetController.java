56ef948e5e758904d82e8604e107d2ca13c3fda4
Dennis Carrasquillo <spycydennis@gmail.com>
2021-10-09 22:42:36 -0400

    Added more CRUD functionality to the Controller and created the frontend

--- backend/jets/src/main/java/com/dennis/carrasquillo/jets/controllers/JetController.java
+++ backend/jets/src/main/java/com/dennis/carrasquillo/jets/controllers/JetController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,5 +63,17 @@ public class JetController {
 		Jet jet = jetRepository
 				.findById(id).orElseThrow(()-> new ResourceNotFoundException("The Jet with this id: " + id + " does not exist!"));
 		return ResponseEntity.ok(jet);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PutMapping("/{id}")
+	public ResponseEntity<Jet> updateJet(@PathVariable int id, @RequestBody Jet jetDetails) {
+		Jet jet = jetRepository
+				.findById(id).orElseThrow(()-> new ResourceNotFoundException("The Jet with this id: " + id + " does not exist!"));
+		jet.setJetName(jetDetails.getJetName());
+		jet.setSpeed(jetDetails.getSpeed());
+		jet.setIntroduced(jetDetails.getIntroduced());
+		Jet updateJet = jetRepository.save(jet);
+		return ResponseEntity.ok(updateJet);
 	}
 }
